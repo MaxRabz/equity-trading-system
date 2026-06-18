4f769296180a6587e69f02a701a93283ff9f954a
Max Rabinowitz <Axrabz9@gmail.com>
2026-06-18 14:39:53 -0400

    Commented the main.py

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -49,13 +49,16 @@ redis_client = AsyncRedis(host=redis_host, port=redis_port_number, db=0)
 # Login details
 # region
 
-class RegisterRequest(BaseModel):
+
+class RegisterRequest(BaseModel):  # Register class for json body
     username: str
     password: str
 
-class LoginRequest(BaseModel):
+
+class LoginRequest(BaseModel):  # Login class for json body
     username: str
     password: str
+
 
 @app.post("/register")
 async def register_user(request: RegisterRequest, response: Response):
@@ -183,6 +186,7 @@ async def create_account(can_short: bool, username: str = Depends(verify_cookie)
 @app.post("/users/accounts/{account_id}")
 async def add_account(account_id: str, username: str = Depends(verify_cookie)):
 
+    # Get account to ensure it exists
     raw_account = await redis_client.hget(redis_dictionaries[1], account_id)
     if not raw_account:
         raise HTTPException(status_code=404, detail="This account does not exist")
@@ -207,11 +211,13 @@ async def add_account(account_id: str, username: str = Depends(verify_cookie)):
 @app.get("/positions")
 async def get_users_positions(username: str = Depends(verify_cookie)):
 
+    # Get User data to check their accounts
     raw_user = await redis_client.hget(redis_dictionaries[0], username)
     user_data = json.loads(raw_user)
 
     positions = {}
 
+    # Grab all positions for checking
     raw_positions = await redis_client.hgetall(redis_dictionaries[3])
 
     for x in raw_positions.values():
@@ -244,8 +250,11 @@ async def get_users_positions(username: str = Depends(verify_cookie)):
 
 
 @app.get("/positions/accounts/{account_id}")
-async def get_accounts_positions(account_id: str, username: str = Depends(verify_cookie)):
+async def get_accounts_positions(
+    account_id: str, username: str = Depends(verify_cookie)
+):
 
+    # Get User data
     raw_user = await redis_client.hget(redis_dictionaries[0], username)
     user_data = json.loads(raw_user)
 
@@ -257,6 +266,7 @@ async def get_accounts_positions(account_id: str, username: str = Depends(verify
 
     positions = {}
 
+    # Grab all positions for checking
     raw_positions = await redis_client.hgetall(redis_dictionaries[3])
 
     for x in raw_positions.values():
@@ -272,18 +282,22 @@ async def get_accounts_positions(account_id: str, username: str = Depends(verify
 
 
 @app.get("/positions/ticker/{ticker}")
-async def get_users_positions_for_ticker(ticker: str, username: str = Depends(verify_cookie)):
+async def get_users_positions_for_ticker(
+    ticker: str, username: str = Depends(verify_cookie)
+):
 
     # Confirm it's a real ticker
     raw_ticker = await redis_client.hget(redis_dictionaries[2], ticker)
     if not raw_ticker:
         raise HTTPException(status_code=404, detail="This ticker does not exist")
 
+    # Get User data
     raw_user = await redis_client.hget(redis_dictionaries[0], username)
     user_data = json.loads(raw_user)
 
     positions = {}
 
+    # Grab all positions for checking
     raw_positions = await redis_client.hgetall(redis_dictionaries[3])
 
     for x in raw_positions.values():
@@ -314,6 +328,7 @@ async def get_accounts_positions_for_ticker(
     if not raw_ticker:
         raise HTTPException(status_code=404, detail="This ticker does not exist")
 
+    # Grab User data
     raw_user = await redis_client.hget(redis_dictionaries[0], username)
     user_data = json.loads(raw_user)
 
@@ -325,6 +340,7 @@ async def get_accounts_positions_for_ticker(
 
     positions = {}
 
+    # Grab all positions for checking
     raw_positions = await redis_client.hgetall(redis_dictionaries[3])
 
     for x in raw_positions.values():
@@ -344,6 +360,8 @@ async def get_accounts_positions_for_ticker(
 # Trades
 # region
 
+
+# Create Trade class so can verify the data in json not query and to ensure all fields are filled out
 class Trade(BaseModel):
     account_id: str
     user_id: str
@@ -353,35 +371,39 @@ class Trade(BaseModel):
     price: str
     other_account: str | None = None
 
+
 @app.post("/trade")
 async def create_trade(trade: list[Trade], username: str = Depends(verify_cookie)):
 
-    if len(trade) == 0:
+    if len(trade) == 0:  # Didn't send any trade data
         raise HTTPException(status_code=422, detail="Invalid Trade Data")
 
-    for trade_item in trade:
-        await individual_trade(username, trade_item.model_dump())
+    for trade_item in trade:  # Loop through each trade one at a time
+        await individual_trade(
+            username, trade_item.model_dump()
+        )  # Converts from class to dictionary for sorting
 
     return {"status": "success"}
 
 
 async def individual_trade(username: str, trade: dict):
 
+    # Ensure it's a valid user
     raw_user = await redis_client.hget(redis_dictionaries[0], username)
     if not raw_user:
         raise HTTPException(status_code=404, detail="This user does not exist")
     user_data = json.loads(raw_user)
 
-    raw_account = await redis_client.hget(
-        redis_dictionaries[1], trade["account_id"]
-    )
+    # Ensure it's a valid account
+    raw_account = await redis_client.hget(redis_dictionaries[1], trade["account_id"])
     if not raw_account:
         raise HTTPException(status_code=404, detail="This account does not exist")
     account_data = json.loads(raw_account)
 
-    if trade["user_id"] != username:
+    # Ensure you are trading for you
+    if trade["user_id"] != user_data["user_id"]:
         raise HTTPException(
-            status_code=401, detail="This user does not match your username"
+            status_code=401, detail="This user_id does not match your user_id"
         )
 
     # Confirm you have access to this account
@@ -395,12 +417,15 @@ async def individual_trade(username: str, trade: dict):
     if not raw_ticker:
         raise HTTPException(status_code=404, detail="This ticker does not exist")
 
+    # Ensure calid direction
     if trade["direction"] not in ("Buy", "Sell"):
         raise HTTPException(status_code=422, detail="Not a valid Direction")
 
+    # Ensure valid quantity
     if trade["quantity"] < 0:
         raise HTTPException(status_code=422, detail="Not a valid quantity value")
 
+    # Grab all positions for editing
     raw_positions = await redis_client.hgetall(redis_dictionaries[3])
     position_key = None
     new_position = None
@@ -408,18 +433,18 @@ async def individual_trade(username: str, trade: dict):
     positions = {
         key.decode() if isinstance(key, bytes) else key: json.loads(value)
         for key, value in raw_positions.items()
-    }
+    }  # Turn all positions into valid dictionaries and not bytes
 
     for key, x in positions.items():
         if (
             x["Account_id"] == trade["account_id"] and x["Ticker"] == trade["ticker"]
         ):  # Correct account and ticker
-            position_key = key
+            position_key = key  # Grab the key of the position to edit
             if (
                 trade["direction"] == "Sell"
                 and x["Quantity"] - trade["quantity"] < 0
                 and not account_data["can_short"]
-            ):
+            ):  # Check if trying to short
                 raise HTTPException(
                     status_code=403, detail="You do not have permission to short"
                 )
@@ -427,11 +452,11 @@ async def individual_trade(username: str, trade: dict):
                 x["Quantity"] + trade["quantity"]
                 if trade["direction"] == "Buy"
                 else x["Quantity"] - trade["quantity"]
-            )
+            )  # Save what the new position will be
 
             break  # only one account and one ticker
 
-    if new_position is None:
+    if new_position is None:  # This position does not currently exist
         if trade["direction"] != "Buy" and not account_data["can_short"]:
             raise HTTPException(
                 status_code=403, detail="You do not have permission to short"
@@ -457,7 +482,7 @@ async def individual_trade(username: str, trade: dict):
 
     now = datetime.now(timezone.utc).isoformat()
 
-    if position_key is None:
+    if position_key is None:  # The position does not exist, create new one
         new_position = (
             trade["quantity"] if trade["direction"] == "Buy" else 0 - trade["quantity"]
         )
@@ -469,15 +494,17 @@ async def individual_trade(username: str, trade: dict):
             "Created_at": now,
             "Updated_at": now,
         }
-        await redis_client.hset(
+        await redis_client.hset(  # Set the new position
             redis_dictionaries[3], position_key, json.dumps(position_data)
         )
-    else:
+    else:  # Editing existing position
+        # Grab the existing positions data
         raw_specific_position = await redis_client.hget(
             redis_dictionaries[3], position_key
         )
         specific_position = json.loads(raw_specific_position)
 
+        # Edit the existing position data
         specific_position["Quantity"] = new_position
         specific_position["Updated_at"] = now
         await redis_client.hset(
@@ -488,7 +515,6 @@ async def individual_trade(username: str, trade: dict):
     await redis_client.xadd("trade_stream", {"d": packed_bytes})
 
     return {"status": "success"}
-
 
 
 # endregion
